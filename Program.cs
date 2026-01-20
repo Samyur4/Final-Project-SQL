479f7db6455093c3388bfed916f0fb27891fc747
Samyur4 <nicksamkurashviliv2@gmail.com>
2026-01-20 18:15:58 +0400

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,15 +1,27 @@
 ﻿using Final_Project_1.Data;
 using Final_Project_1.Models;
+using Microsoft.EntityFrameworkCore;
 
 using var db = new LibraryDbContext();
 
-var newMember = new Member { FirstName = "Nikoloz", LastName = "T", Email = "niko@example.com" };
-var newGenre = new Genre { Name = "Tech" };
-var newStaff = new Staff { FullName = "Lasha T", Position = "Security" };
+if (!db.Members.Any(m => m.Email == "niko@example.com"))
+{
+    var newMember = new Member { FirstName = "Nikoloz", LastName = "T", Email = "niko@example.com" };
+    db.Members.Add(newMember);
+}
 
-db.Members.Add(newMember);
-db.Genres.Add(newGenre);
-db.Staff.Add(newStaff);
+if (!db.Genres.Any(g => g.Name == "Tech"))
+{
+    var newGenre = new Genre { Name = "Tech" };
+    db.Genres.Add(newGenre);
+}
+
+if (!db.Staff.Any(s => s.FullName == "Lasha T"))
+{
+    var newStaff = new Staff { FullName = "Lasha T", Position = "Security" };
+    db.Staff.Add(newStaff);
+}
+
 db.SaveChanges();
 
 var filteredBooks = db.Books
@@ -39,7 +51,7 @@ var bookDetails = db.Books
     }).ToList();
 
 var genreStats = db.Books
-    .GroupBy(b => b.Genre.Name)
+    .GroupBy(b => b.Genre != null ? b.Genre.Name : "No Genre")
     .Select(g => new {
         Genre = g.Key,
         Count = g.Count()
